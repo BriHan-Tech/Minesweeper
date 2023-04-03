05a48eaadd9e651d45aabaa9421dccb4e8f4c667
Brian Han <brihan.tech@gmail.com>
2023-04-03 00:55:31 -0700

    ⚡ - Creating Cell Component

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import iCell from "../../models/iCell";
 import "./Cell.scss";
 
-const Cell = () => {
-  return <div className="cell"></div>;
+const Cell = (props: iCell) => {
+  const [isClicked, setIsClicked] = useState<boolean>(true);
+  const [isRightClicked, setIsRightClicked] = useState<boolean>(false);
+
+  const handleClick = () => {
+    if (isRightClicked) return;
+    setIsClicked(true);
+  };
+
+  const handleRightClick = (e: any) => {
+    e.preventDefault();
+    if (isClicked) return;
+    setIsRightClicked((prev: boolean) => !prev);
+  };
+
+  return (
+    <div
+      className="cell"
+      style={{
+        backgroundColor: props.isClear ? "white" : "#AAD751",
+      }}
+      onClick={handleClick}
+      onContextMenu={handleRightClick}
+    >
+      {isClicked && <span>{props.isMine ? "💣" : props.numMines}</span>}
+      {isRightClicked && <span>🚩</span>}
+    </div>
+  );
 };
 
 export default Cell;
