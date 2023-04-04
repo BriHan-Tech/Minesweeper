0cfe04cffd255e775bde821493138b4b7ff2deef
Brian Han <brihan.tech@gmail.com>
2023-04-04 03:43:59 -0700

    📚 - Added interface for position objects

---
+++ src/models/iPos.ts
@@ -0,0 +1,9 @@
+/**
+ * Interface for an object that contains data about the x and y position.
+ *    x: number                 Col the cell is in
+ *    y: number                 Row the cell is in
+ */
+export default interface iPos {
+  x: number;
+  y: number;
+}
