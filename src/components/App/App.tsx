ec33369e0bd38170a6f7eead4a026328148cfbef
Brian Han <brihan.tech@gmail.com>
2023-04-06 03:31:39 -0700

    ✨ - Added number of mines to toolbar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState, createContext } from "react";
-import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
-import "./App.scss";
 import Board from "../Board/Board";
 import Timer from "../Timer/Timer";
 import Settings from "../Settings/Settings";
+import GameFinishedPopup from "../GameFinishedPopup/GameFinishedPopup";
 import { GameContext } from "./GameContext";
 import { GameSettingsContext } from "./GameSettingsContext";
 import { iGameSettings } from "../../interfaces/iGameSettings";
-import GameFinishedPopup from "../GameFinishedPopup/GameFinishedPopup";
+import { CELL_SIZE } from "../../constants";
+import reset from "../../assets/imgs/reset.svg";
+import mineLineArt from "../../assets/imgs/mine-lineart.svg";
+import "./App.scss";
 
 const App = () => {
   // "" = Game has not started
@@ -32,16 +35,36 @@ const App = () => {
         </AnimatePresence>
 
         <div className="container">
-          <div className="toolbar">
-            <Timer />
-            <button
-              className="toolbar__reset_button"
+          <div
+            className="toolbar"
+            style={{
+              width: `${CELL_SIZE * gameSettings.COLS}px`,
+            }}
+          >
+            <div className="toolbar__left">
+              <span className="toolbar__left__mine-count">
+                <img
+                  src={mineLineArt}
+                  alt="mines"
+                  className="toolbar__left__mine-count__icon"
+                />
+                {gameSettings.NUM_MINES}
+              </span>
+              <Timer />
+            </div>
+            <motion.button
+              className="toolbar__reset-btn"
+              style={{
+                backgroundImage: `url(${reset})`,
+              }}
+              whileHover={{
+                rotate: 360,
+                transition: { duration: 0.1, ease: "linear" },
+              }}
               onClick={() => {
                 setGameStatus("");
               }}
-            >
-              Reset
-            </button>
+            ></motion.button>
           </div>
           <Board />
         </div>
