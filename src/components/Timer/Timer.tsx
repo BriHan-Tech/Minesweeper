2722c24ea7ae1e1339fe20a232405fb8bc29c667
Brian Han <brihan.tech@gmail.com>
2023-04-06 03:28:46 -0700

    🎨 - Toolbar style update

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,21 +1,33 @@
 import { useState, useEffect, useContext } from "react";
 
-import "./Timer.scss";
 import { GameContext } from "../App/GameContext";
+import stopwatch from "../../assets/imgs/stopwatch.svg";
+import "./Timer.scss";
 
 const Timer = () => {
   const [seconds, setSeconds] = useState<number>(0);
-  const { gameStatus, setGamestatus } = useContext(GameContext);
+  const { gameStatus } = useContext(GameContext);
 
   useEffect(() => {
     let intervalId: any;
-    if (gameStatus.length > 0 && gameStatus != "L" && gameStatus != "W")
+
+    // If the game started & the game is not finished
+    if (gameStatus.length > 0 && gameStatus !== "L" && gameStatus !== "W")
       intervalId = setInterval(() => setSeconds((prev) => prev + 1), 1000);
-    if (gameStatus == "R") setSeconds(0);
+
+    // If the timer is game is getting reset
+    if (gameStatus === "S") setSeconds(0);
+
+    // Clear interval
     return () => clearInterval(intervalId);
   }, [gameStatus]);
 
-  return <span className="timer">{seconds}</span>;
+  return (
+    <div className="timer">
+      <img className="timer__icon" src={stopwatch} />
+      {seconds}s
+    </div>
+  );
 };
 
 export default Timer;
